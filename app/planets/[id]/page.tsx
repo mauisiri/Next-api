2205ca869b121dfd178e4d301adec788b6de59c8
mauisiri <irisleo.com@gmail.com>
2023-07-14 13:00:26 +0200

    Fetching, but I have to make the AllPlanets work as a component

--- app/planets/[id]/page.tsx
+++ app/planets/[id]/page.tsx
@@ -1,4 +1,4 @@
-export default function Planet ({ params }) {
+export default function Results ({ params }) {
     const { id } = params
     
     return <h1>{id} - Mercurio </h1>
--- app/planets/page.tsx
+++ app/planets/page.tsx
@@ -1,33 +1,10 @@
-const fetchPlanets = () => {
-  return fetch('https://swapi.dev/api/planets')
-    .then(res => res.json())
-}
-export default async function PlanetsPage({ params }) {
-  const planets = await fetchPlanets()
+import AllPlanets from "@/components/AllPlanets";
 
+export default async function PlanetsPage({ params }) {
   return (
     <section>
-    {planets.map(planet => (
-        <article key={planet.id}>
-            <h2>{planet.name}</h2>
-            <p>{planet.rotation_period}</p>
-            <p>{planet.diameter}</p>
-            <p>{planet.climate}</p>
-            <p>{planet.gravity}</p>
-            <p>{planet.terrain}</p>
-            <p>{planet.surface_water}</p>
-            <p>{planet.population}</p>
-            <p>{planet.residents}</p>
-            <p>{planet.films}</p>
-            <p>{planet.created}</p>
-            <p>{planet.edited}</p>
-            <p>{planet.url}</p>
-        </article>
-    ))}
+    <AllPlanets />
     </section>
   )
 }
-
-
-
-
+ 
